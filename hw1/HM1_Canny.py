4117835c9e5d000a4f9d18b0dd462f16d8bc2e84
jellllly420 <z2629365594@gmail.com>
2023-03-27 05:09:55 +0800

    hw1 task2 finished

--- hw1/HM1_Canny.py
+++ hw1/HM1_Canny.py
@@ -1,5 +1,5 @@
 import numpy as np
-from HM1_Convolve import Gaussian_filter, Sobel_filter_x, Sobel_filter_y
+from HM1_Convolve import Gaussian_filter, Sobel_filter_x, Sobel_filter_y, padding
 from utils import read_img, write_img
 
 def compute_gradient_magnitude_direction(x_grad, y_grad):
@@ -12,8 +12,9 @@ def compute_gradient_magnitude_direction(x_grad, y_grad):
             magnitude_grad: array(float)
             direction_grad: array(float) you may keep the angle of the gradient at each pixel
     """
+    #compute
     magnitude_grad = np.sqrt(x_grad * x_grad + y_grad * y_grad)
-    direction_grad = np.argtan2(y_grad, x_grad)
+    direction_grad = np.arctan2(x_grad, y_grad)
 
     return magnitude_grad, direction_grad 
 
@@ -27,7 +28,7 @@ def non_maximal_suppressor(grad_mag, grad_dir):
             grad_dir: array(float)
         Outputs:
             output: array(float)
-    """ 
+    """
     #compute step size
     x_step = np.cos(grad_dir) * grad_mag
     y_step = np.sin(grad_dir) * grad_mag
@@ -40,15 +41,33 @@ def non_maximal_suppressor(grad_mag, grad_dir):
     y_neagtive_neighbour = np.arange(ncol) - y_step
 
     def bilinear_interpolation(x, y):
-        up = np.floor(x)
-        down = up + 1
-        left = np.floor(y)
-        right = left + 1
+        #compute raw border
+        raw_up = np.floor(x).astype(int)
+        raw_down = (raw_up + 1).astype(int)
+        raw_left = np.floor(y).astype(int)
+        raw_right = (raw_left + 1).astype(int)
 
-        grad_mag
+        #compute borders normalized into the img shape
+        up = np.where(raw_up >= nrow, nrow - 1, np.where(raw_up < 0, 0, raw_up)).astype(int)
+        down = np.where(raw_down >= nrow, nrow - 1, np.where(raw_down < 0, 0, raw_down)).astype(int)
+        left = np.where(raw_left >= ncol, ncol - 1, np.where(raw_left < 0, 0, raw_left)).astype(int)
+        right = np.where(raw_right >= ncol, ncol - 1, np.where(raw_right < 0, 0, raw_right)).astype(int)
+
+        #compute output
+        output = (down - x) * ((y - left) * grad_mag[up, right] + (right - y) * grad_mag[up, left]) + \
+                 (x - up) * ((y - left) * grad_mag[down, right] + (right - y) * grad_mag[down, left])
+        
+        #dropout the points whose neighbours cannot be reached
+        output = np.where((raw_up >= 0) & (raw_down < nrow) & (raw_left >= 0) & (raw_right < ncol), output, 0)
+
+        return output
+    
+    #suppression
+    NMS_output = np.where((bilinear_interpolation(x_postive_neighbour, y_postive_neighbour) <= grad_mag) &
+                          (bilinear_interpolation(x_negative_neighbour, y_neagtive_neighbour) <= grad_mag),
+                          grad_mag, 0)
 
     return NMS_output 
-            
 
 
 def hysteresis_thresholding(img) :
@@ -62,8 +81,54 @@ def hysteresis_thresholding(img) :
 
 
     #you can adjust the parameters to fit your own implementation 
-    low_ratio = 0.10
-    high_ratio = 0.30
+    low_ratio = 0.8
+    high_ratio = 1.0
+
+    #compute threshold
+    print(np.mean(img))
+    mean = np.sum(img) / np.sum(img != 0)
+    low_threshold = low_ratio * mean
+    high_threshold = high_ratio * mean
+
+    #compute size
+    nrow, ncol = img.shape
+
+    #dropout and reserve
+    #print(img)
+    img = np.where(img <= low_threshold, 0, img)
+    #print(img)
+    output = np.where(img >= high_threshold, 1, 0)
+
+    """
+    Switched to the strategy stated in 
+    https://towardsdatascience.com/canny-edge-detection-step-by-step-in-python-computer-vision-b49c3a2d8123
+    from that in slides because of the lack of direction parameter.
+    """
+    #grow edges
+    updated = True
+    while updated:
+        updated = False
+
+        #compute dirs
+        dirs = [[-1, 0], [-1, 1], [0, 1], [1, 1],
+                [1, 0], [1, -1], [0, -1], [-1, -1]]
+        
+        #update
+        for i in range(nrow):
+            for j in range(ncol):
+                if output[i, j] or not img[i, j]:
+                    continue
+                for i_dir, j_dir in dirs:
+                    i_neighbour, j_neighbour = i + i_dir, j + j_dir
+                    if i_neighbour < 0 or i_neighbour >= nrow or j_neighbour < 0 or j_neighbour >= ncol:
+                        continue
+                    if output[i_neighbour, j_neighbour]:
+                        output[i, j] = 1
+                        updated = True
+                        break
+    
+    #update stable weak points to 0
+    output = np.where((img > 0) & np.logical_not(output), 0, output)
     
     return output 
 
